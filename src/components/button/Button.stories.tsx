568741a8e8566df94ba1423317e60a8249f00f6f
Arvind <arvind.rathour06@gmail.com>
2024-07-02 13:33:15 +0530

    added css to button

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -4,7 +4,7 @@ import  Button  from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/MyButton',
+  title: 'ReactComponentLibrary/MyButton',
   component: Button,
 
 } satisfies Meta<typeof Button>;
